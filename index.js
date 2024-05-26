82c58494c62f4239ab30a8e19165b420a118b3d0
Gerson Sanchez <gealsanchez@gmail.com>
2024-05-26 00:08:13 -0500

    emailjs setup

--- index.js
+++ index.js
@@ -4,9 +4,24 @@ import auth from "json-server-auth";
 
 const server = express();
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Headers', '*')
-    next()
+    res.setHeader(
+        "Access-Control-Allow-Origin",
+        "http://localhost:3000"
+      );
+      res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+      );
+      res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+      );
+      res.setHeader("Access-Control-Allow-Credentials", true);
+      res.setHeader("Access-Control-Allow-Private-Network", true);
+      //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+      res.setHeader("Access-Control-Max-Age", 7200);
+    
+      next();
 })
 
 const router = jsonServer.router('./data/db.json');
