8aeadd6d96ffcebb815d7ace51f73b965fd0ba99
Gerson Sanchez <gealsanchez@gmail.com>
2024-05-29 12:54:04 -0500

    Take one valencia

--- index.js
+++ index.js
@@ -4,24 +4,24 @@ import auth from "json-server-auth";
 
 const server = express();
 server.use((req, res, next) => {
-    res.setHeader(
-        "Access-Control-Allow-Origin",
-        "https://main--officialstore-hibrid-it.netlify.app"
-      );
-      res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-      );
-      res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-      );
-      res.setHeader("Access-Control-Allow-Credentials", true);
-      res.setHeader("Access-Control-Allow-Private-Network", true);
-      //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-      res.setHeader("Access-Control-Max-Age", 7200);
-    
-      next();
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://main--floristeria-valencia.netlify.app"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
+
+  next();
 })
 
 const router = jsonServer.router('./data/db.json');
@@ -30,10 +30,10 @@ server.db = router.db
 
 const middlewares = jsonServer.defaults()
 const rules = auth.rewriter({
-    products: 444,
-    featured_products: 444,
-    orders: 660,
-    users: 600
+  products: 444,
+  featured_products: 444,
+  orders: 660,
+  users: 600
 });
 
 server.use(rules)
